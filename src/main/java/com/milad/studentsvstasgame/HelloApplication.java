9afd0c78a388e4877e6480e8ad08b7a5f208c7f6
miladbila <btavakoli14@gmail.com>
2022-07-04 21:14:03 +0430

    adding game page pointer

--- src/main/java/com/milad/studentsvstasgame/HelloApplication.java
+++ src/main/java/com/milad/studentsvstasgame/HelloApplication.java
@@ -1,18 +1,20 @@
 package com.milad.studentsvstasgame;
 
+import com.milad.studentsvstasgame.heros.student.StudiousStudent;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    static GamePage gamePage;
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        gamePage=new GamePage();
+        Scene scene=new Scene(gamePage);
         stage.setScene(scene);
         stage.show();
     }
